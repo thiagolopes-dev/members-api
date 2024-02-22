33618c1847c4a3bd38494ce3255b01aa30b5fa8d
Thiago Lopes <thiagoolopezz@gmail.com>
2024-02-22 14:07:07 -0300

    add auth authentication

--- core/views.py
+++ core/views.py
@@ -2,10 +2,14 @@ from rest_framework import permissions, viewsets
 from .models import Member
 from .serializers import MemberSerializer, MemberSimpleSerializer
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def list(self, request, *args, **Kwargs):
         queryset = Member.objects.all()
--- front/settings.py
+++ front/settings.py
@@ -40,8 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
